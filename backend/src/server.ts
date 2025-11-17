b0e9e628b271d5ac1389baa55efc047464c6e5b0
Sara Romao <sara-romao@live.com>
2025-11-17 15:40:20 -0300

    config server and api company

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,19 @@
 import fastify from "fastify";
+import { createCompany } from "./http/routes/create-company.js";
+import { getCompany } from "./http/routes/get-company.js";
+
 
 const app = fastify();
 
-app.get('/', async function handler (request, reply) {
-    return { hello: 'world' }
-  })
+
+app.register(createCompany);
+app.register(getCompany);
+
+app.get("/", (request, reply)=>{
+    reply.send({msg:"hello"})
+
+})
+
 
 app.listen({port:8080}).then(()=>{
     console.log('Server is running ');
