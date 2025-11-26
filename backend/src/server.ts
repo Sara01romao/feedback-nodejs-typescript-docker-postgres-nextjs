9d835468f38bc5454bd0d124f3295fa3246c8308
Sara Romao <sara-romao@live.com>
2025-11-25 22:52:56 -0300

    feat: added login route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,18 @@
 import fastify from "fastify";
 import { createCompany } from "./http/routes/create-company.js";
 import { getCompany } from "./http/routes/get-company.js";
-
+import { postFeedback } from "./http/routes/feedback-create.js";
+import { getFeedback } from "./http/routes/get-feedback.js";
+import { login } from "./http/routes/login.js";
 
 const app = fastify();
 
-
 app.register(createCompany);
 app.register(getCompany);
+app.register(postFeedback);
+app.register(getFeedback);
+app.register(login);
 
-app.get("/", (request, reply)=>{
-    reply.send({msg:"hello"})
-
-})
-
-
-app.listen({port:8080}).then(()=>{
+app.listen({ port: 8080 }).then(() => {
     console.log('Server is running ');
 })
