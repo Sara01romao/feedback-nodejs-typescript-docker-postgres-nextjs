486e6331ddd85777ec3db66572084c9fc6bef56f
Sara Romao <sara-romao@live.com>
2025-11-27 13:33:48 -0300

    Removed route filter and updated the feedback route to use optional query parameters for filtering

--- backend/src/http/routes/get-feedback.ts
+++ backend/src/http/routes/get-feedback.ts
@@ -4,47 +4,30 @@ import authMiddleware from "../../middleware/authMiddleware.js";
 import z from "zod";
 
 export async function getFeedback(app: FastifyInstance) {
-
   app.get('/feedbacks', { preHandler: authMiddleware }, async (request, reply) => {
 
-    console.log(request.user.id)
-
-    const feedbacks = await prisma.feedback.findMany({
-      where: {
-        companyId: request.user.id,
-      },
-      orderBy: {
-        createdAt: 'asc'
-      }
-    });
-
-    console.log(feedbacks)
-
-    return reply.send({ feedbacks: feedbacks });
-  });
-
-  app.get('/feedbacks/filter', { preHandler: authMiddleware }, async (request, reply) => {
-
     const filterBody = z.object({
-       type: z.enum(['sugestao', 'critica', 'comentario']),
-       order:z.enum(['desc', 'asc'])
-    })
-   
-    const data = filterBody.parse(request.query);
+      type: z.enum(['sugestao', 'critica', 'comentario']).optional(),
+      order: z.enum(['desc', 'asc']).optional(),
+      date: z.coerce.date().optional()
+    });
+    const filter = filterBody.parse(request.query);
     const feedbacks = await prisma.feedback.findMany({
       where: {
         companyId: request.user.id,
-        type: data.type
+        ...(filter.type && { type: filter.type }),
+        ...(filter.date && {
+          createdAt: {
+            gte: new Date(filter.date.toISOString().split('T')[0] + "T00:00:00.000Z"),
+            lt: new Date(filter.date.toISOString().split('T')[0] + "T23:59:59.999Z")
+          }
+        })
       },
       orderBy: {
-        createdAt: data.order ?? 'desc'
+        createdAt: filter.order ?? "desc",
       }
     });
 
-    console.log(feedbacks)
-
-    return reply.send({ feedbacks: feedbacks });
-  })
-
-
+    return reply.send({ codeCompany: request.user.code, feedbacks: feedbacks});
+  });
 }
