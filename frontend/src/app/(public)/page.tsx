e2eee04e375d0b22afbe9c51d3b0ba9982dc4279
Sara Romao <sara-romao@live.com>
2025-12-11 15:56:40 -0300

    config base to public routes

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -1,9 +1,10 @@
+import { FeedbackForm } from "@/src/components/forms/feedback-form";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans ">
-
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between p-4 sm:items-start">
-        home
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center  p-4 sm:items-start">
+        <FeedbackForm/>
       </main>
     </div>
   );
