9b6425e27aa1257c1a4929eafd051b71025ab038
Sara Romao <sara-romao@live.com>
2025-12-17 12:43:15 -0300

    modified button form

--- frontend/src/components/forms/feedback-form.tsx
+++ frontend/src/components/forms/feedback-form.tsx
@@ -6,6 +6,7 @@ import Textarea from "../textarea";
 import Select from "../select";
 import { postFeedback } from "@/src/actions/post-feedback";
 import { useModal } from "../modal";
+import { SubmitButton } from "../btn-submit";
 
 export function FeedbackForm() {
   const { openModal } = useModal();
@@ -19,11 +20,10 @@ export function FeedbackForm() {
   });
 
   useEffect(() => {
-    console.log("estado", state);
     if (state.ok) {
       openModal(
         <div className="flex flex-col items-center justify-center gap-4">
-          <svg width="25" height="19" viewBox="0 0 25 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg className="animate-bounce" width="25" height="19" viewBox="0 0 25 19" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M8.4911 18.2766L0.366101 10.1516C-0.122034 9.66351 -0.122034 8.87206 0.366101 8.38387L2.13383 6.6161C2.62196 6.12792 3.41346 6.12792 3.9016 6.6161L9.37499 12.0894L21.0984 0.366101C21.5865 -0.122034 22.378 -0.122034 22.8662 0.366101L24.6339 2.13387C25.122 2.62201 25.122 3.41346 24.6339 3.90165L10.2589 18.2767C9.77069 18.7648 8.97924 18.7648 8.4911 18.2766Z" fill="#00B244" />
           </svg>
           <h2 className="text-xl font-semibold text-center text-[#643EE4]">Feedback Enviado </h2>
@@ -52,7 +52,7 @@ export function FeedbackForm() {
           <p className="text-red-500 mt-2">{state.error}</p>
         )}
 
-        <button type="submit" className="bg-[#643EE4] text-white p-2 rounded-md cursor-pointer hover:bg-[#5535C3] duration-300 ease-in-out">Enviar</button>
+        <SubmitButton />
       </form>
     </>
   )
