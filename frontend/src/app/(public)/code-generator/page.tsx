afaf74b29371297a0eb9262b82e04b3771856e15
Sara Romao <sara-romao@live.com>
2025-12-20 16:02:28 -0300

    modified structure code generator page

--- frontend/src/app/(public)/code-generator/page.tsx
+++ frontend/src/app/(public)/code-generator/page.tsx
@@ -1,6 +1,11 @@
+import { CodeForm } from "@/src/components/forms/code-generator-form";
 
-export default function Code(){
-  return(
-    <h1>code</h1>
+export default function Code() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-sans ">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center  p-4 sm:items-start">
+        <CodeForm/>
+      </main>
+    </div>
   )
 }
