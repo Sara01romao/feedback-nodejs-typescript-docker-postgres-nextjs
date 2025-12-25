29e27a1876a651c0415167555026ca2a7b66ea24
Sara Romao <sara-romao@live.com>
2025-12-24 23:51:53 -0300

    added type data api and component card

--- frontend/src/app/(auth)/dashboard/page.tsx
+++ frontend/src/app/(auth)/dashboard/page.tsx
@@ -1,13 +1,30 @@
-import { logout } from "@/src/actions/logout";
+import { Card } from "@/src/components/card-feedback";
 import { validateToken } from "@/src/function/validate-token";
 
+export type  FeedBack = {
+  id: string;
+  type: string;
+  description: string;
+  createdAt: Date;
+}
+type feedbackType = {
+  codeCompany: string,
+  feedbacks: FeedBack[]
+}
+
 export default async function Dashboard() {
-  const userData = await validateToken();
-  console.log(userData)
+  const { data }:{data:feedbackType} = await validateToken();
+
   return (
     <div className="flex min-h-screen items-center justify-center font-sans ">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center  p-4 sm:items-start">
         <h1 className="text-lg">Dashboar</h1>
+
+        <div className="w-full flex flex-col gap-2">
+          {
+            data.feedbacks.map((feedback) => (<Card key={feedback.id} id={feedback.id} type={feedback.type} description={feedback.description} createdAt={feedback.createdAt}/>))
+          }
+        </div>
       </main>
     </div>
   )
