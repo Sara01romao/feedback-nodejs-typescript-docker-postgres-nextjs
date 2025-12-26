ac840165866270baf38b0b9bd2a2ccbb98bb2368
Sara Romao <sara-romao@live.com>
2025-12-25 22:34:01 -0300

    adjustment layout page added component and types

--- frontend/src/app/(auth)/dashboard/page.tsx
+++ frontend/src/app/(auth)/dashboard/page.tsx
@@ -1,30 +1,34 @@
 import { Card } from "@/src/components/card-feedback";
+import { Filter } from "@/src/components/forms/filter-form";
+import { ListFeedbacks } from "@/src/components/list-feedbacks";
 import { validateToken } from "@/src/function/validate-token";
 
-export type  FeedBack = {
+export type FeedBack = {
   id: string;
   type: string;
   description: string;
-  createdAt: Date;
+  createdAt: string;
+  companyId: string;
 }
-type feedbackType = {
+export type feedbackType = {
   codeCompany: string,
   feedbacks: FeedBack[]
 }
 
 export default async function Dashboard() {
-  const { data }:{data:feedbackType} = await validateToken();
+
+  const { data }: { data: feedbackType } = await validateToken();
 
   return (
     <div className="flex min-h-screen items-center justify-center font-sans ">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center  p-4 sm:items-start">
-        <h1 className="text-lg">Dashboar</h1>
+       <div>
+        
+       </div>
 
-        <div className="w-full flex flex-col gap-2">
-          {
-            data.feedbacks.map((feedback) => (<Card key={feedback.id} id={feedback.id} type={feedback.type} description={feedback.description} createdAt={feedback.createdAt}/>))
-          }
-        </div>
+        <section className="flex items-start w-full gap-4">
+          <ListFeedbacks feedbacks={data.feedbacks} />
+        </section>
       </main>
     </div>
   )
