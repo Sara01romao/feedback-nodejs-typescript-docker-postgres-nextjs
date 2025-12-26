14a1c2df44eccf71b305c3ae5bffcdcef3a55018
Sara Romao <sara-romao@live.com>
2025-12-25 22:34:55 -0300

    text button dynamic

--- frontend/src/components/btn-submit.tsx
+++ frontend/src/components/btn-submit.tsx
@@ -2,14 +2,17 @@
 
 import { useFormStatus } from 'react-dom';
 
-export function SubmitButton() {
+type Button ={
+  text?: string;
+}
+export function SubmitButton({text}:Button) {
   const { pending } = useFormStatus();
 
   return (
     <button
       type="submit" disabled={pending}
-      className={`${pending ?"bg-[#5535C3]" :"bg-[#643EE4]"}  text-white p-2 cursor-pointer rounded-md hover:bg-[#5535C3] duration-300 ease-in-out disabled:opacity-50 disabled:cursor-not-allowed`}>
-      {pending ? 'Enviando...' : 'Enviar'}
+      className={`${pending ?"bg-[#5535C3]" :"bg-[#643EE4] w-full"}  text-white p-2 cursor-pointer rounded-md hover:bg-[#5535C3] duration-300 ease-in-out disabled:opacity-50 disabled:cursor-not-allowed`}>
+      {pending ? 'Enviando...' : text ?? "Enviando"}
     </button>
   );
 }
