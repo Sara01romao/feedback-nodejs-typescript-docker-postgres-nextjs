b870399b91d62bcd94ae39adb0182edf35b507a8
Sara Romao <sara-romao@live.com>
2025-12-28 00:16:51 -0300

    remove option

--- frontend/src/components/select.tsx
+++ frontend/src/components/select.tsx
@@ -15,7 +15,6 @@ export default function Select({options, name, title, change}:Select){
    <div className="flex flex-col w-full">
     <label htmlFor={name} className="text-md font-medium text-gray-700">{title}</label>
       <select className="border border-[#A6A6A6]  rounded-sm p-1 px-2" name={name} onChange={(e) => change(e.target.value)}>
-        <option value="">Selecione</option>
         {options.map(option => <option value={option.value} key={option.label}>{option.label}</option>)}
       </select>
    </div>
