772c8dfe13618f545e84dae2643e0283abd28511
Sara Romao <sara-romao@live.com>
2025-12-28 00:08:48 -0300

    ajuste na remoção do token

--- frontend/src/actions/logout.ts
+++ frontend/src/actions/logout.ts
@@ -5,6 +5,7 @@ import { redirect } from 'next/navigation';
 export async function logout() {
   const cookieStore = await cookies(); 
  
+
   cookieStore.delete('token');
   cookieStore.delete('session');
   cookieStore.delete('refreshToken');
